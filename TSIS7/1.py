41e980eb1bea0cb631415c3184f7153113079f88
Alikhan Bissakov <bissakov@yandex.ru>
2021-04-04 21:29:49 +0600

    .

--- TSIS7/1.py
+++ TSIS7/1.py
@@ -14,18 +14,36 @@ screen = pygame.display.set_mode((w + 160, h))
 
 screen.fill([255, 255, 255])
 
-def clearFromBord(surface,col,thickness):
+def clear(surface,col,thickness):
     pygame.draw.rect(surface, col, pygame.Rect(0, 0, w, thickness))
     pygame.draw.rect(surface, col, pygame.Rect(0, 0, thickness, h))
     pygame.draw.rect(surface, col, pygame.Rect(w-thickness, 0, thickness + 130, h))
     pygame.draw.rect(surface, col, pygame.Rect(0, h-thickness, w, thickness))
     #pygame.draw.rect(surface, col, pygame.Rect(w, h-thickness, w, thickness))
 
+def annotation(s,x,y,col,sz):
+    font = pygame.font.Font("arial.ttf", sz, bold=False)
+    text = font.render(s, True, col)
+    screen.blit(text,(x,y))
+
 def drawBorder(surface,col,thickness):
-    pygame.draw.rect(surface, col, pygame.Rect(30, 30, w, thickness))
-    pygame.draw.rect(surface, col, pygame.Rect(30, 30, thickness, h))
-    pygame.draw.rect(surface, col, pygame.Rect(w-thickness-30, 0, thickness, h))
-    pygame.draw.rect(surface, col, pygame.Rect(0, h-thickness-30, w, thickness))
+    pygame.draw.line(surface, col, (30,30), (w-30,30),thickness)
+    pygame.draw.line(surface, col, (30,30), (30,h-30),thickness)
+    pygame.draw.line(surface, col, (30,h-30), (w-30,h-30),thickness)
+    pygame.draw.line(surface, col, (w-30,30), (w-30,h-30),thickness)
+    
+    vbound = h - 39.8
+    hbound = 560
+    j = -1.00
+    # pygame.draw.line(surface, col, (0,440.2), (w-thickness-30,440.2),2)
+    # pygame.draw.line(surface, col, (0,340.2), (w-thickness-30,340.2),2)
+    for i in range(0,9):
+        pygame.draw.line(surface, col, (30,vbound - 50*i), (w - 30,vbound - 50*i),1)
+        annotation(str(format(j, '.2f')),5,vbound - 50*i - 7,black,10)
+        j += 0.25
+        if i < 7:
+            pygame.draw.line(surface, col, (hbound - 80*i,30), (hbound - 80*i,h - 30),1)
+
 
 def writePi(x):
     num = str(Fraction(x, 2))
@@ -39,20 +57,19 @@ def drawPlot():
     
     i = h/2 + 15.4
     j = 1
-    while i <= h - 30:
-        if j % 2 != 0: pygame.draw.rect(screen, black, pygame.Rect(24, i, 15, 2))
-        else: pygame.draw.rect(screen, black, pygame.Rect(27, i, 7, 2))
-        #text = font.render("", True, (0, 128, 0))
-        i += 15.4
-        j += 1
+    # while i <= h - 30:
+    #     if j % 2 != 0: pygame.draw.rect(screen, black, pygame.Rect(24, i, 15, 2))
+    #     else: pygame.draw.rect(screen, black, pygame.Rect(27, i, 7, 2))
+    #     i += 15.4
+    #     j += 1
 
-    i = h/2 - 15.4
-    j = 1
-    while i >= 30:
-        if j % 2 != 0: pygame.draw.rect(screen, black, pygame.Rect(24, i, 15, 2))
-        else: pygame.draw.rect(screen, black, pygame.Rect(27, i, 7, 2))
-        i -= 15.4
-        j += 1
+    # i = h/2 - 15.4
+    # j = 1
+    # while i >= 30:
+    #     if j % 2 != 0: pygame.draw.rect(screen, black, pygame.Rect(24, i, 15, 2))
+    #     else: pygame.draw.rect(screen, black, pygame.Rect(27, i, 7, 2))
+    #     i -= 15.4
+    #     j += 1
 
     i = w/2 + 40
     j = 1
@@ -76,21 +93,20 @@ def drawPlot():
         i -= 40
         j += 1
 
-def annotation(s,x,y,col):
-    font = pygame.font.Font("arial.ttf", 32)
-    text = font.render(s, True, col)
-    screen.blit(text,(x,y))
-
 def writeAnnotations():
-    annotation("-",640,50,red)
-    annotation("sinus",660,50,black)
-    annotation("-",640,80,blue)
-    annotation("cosinus",660,80,black)
+    annotation("-",637,50,red,32)
+    annotation("-",645,50,red,32)
+    annotation("sinus",660,50,black,32)
+    annotation("-",635,80,blue,32)
+    annotation("-",647,80,blue,32)
+    annotation("cosinus",660,80,black,32)
+    pygame.draw.line(screen,white,(636,40),(636,120),3)
+    pygame.draw.line(screen,white,(656,40),(656,120),3)
 
 def drawTrig(s):
     plotPoints = []
     if s == "sin":
-        for x in range(0, w):
+        for x in range(80, 561):
             y = int(math.sin(x/640.0 * 8 * math.pi + math.pi) * 200 + 240)
             plotPoints.append([x,y])
         pygame.draw.lines(screen, red, False, plotPoints, 2)
@@ -100,7 +116,7 @@ def drawTrig(s):
         #     except IndexError:
         #         break
     elif s == "cos":
-        for x in range(0, w, 2):
+        for x in range(80, 560, 2):
             y = int(math.cos(x/640.0 * 8 * math.pi + math.pi) * 200 + 240)
             plotPoints.append([x,y])
         for i in range(0,len(plotPoints),2):
@@ -115,18 +131,22 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    
+    clear(screen,white,30)
+    drawBorder(screen,black,2)
+
     drawTrig("sin")
     drawTrig("cos")
 
-    pygame.draw.line(screen, black, (0, h/2), (w, h/2), 2)
-    pygame.draw.line(screen, black, (w/2, 0), (w/2, h), 2)
+    pygame.draw.line(screen, black, (30, h/2), (w-30, h/2), 2)
+    pygame.draw.line(screen, black, (w/2, 30), (w/2, h-30), 2)
 
-    drawBorder(screen,black,1)
-    clearFromBord(screen,white,30)
+    
+    
 
     writeAnnotations()
 
     drawPlot()
+
+    
     
     pygame.display.flip()
