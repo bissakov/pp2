a8c5305a3e4e82d36146cd880356104a4f9b1f46
Alikhan Bissakov <bissakov@yandex.ru>
2021-04-10 22:34:55 +0600

    .

--- TSIS8/game.py
+++ TSIS8/game.py
@@ -1,14 +1,15 @@
-import pygame, sys
+import sys
+import pygame as pg
 import player_car
 import enemy_cars
 import collectable
 import start_screen
 import road_speed
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 960))
-pygame.display.set_caption("Game")
-FPS = pygame.time.Clock()
+pg.init()
+screen = pg.display.set_mode((1280, 960))
+pg.display.set_caption("Game")
+FPS = pg.time.Clock()
 
 menu = start_screen.Menu()
 
@@ -17,45 +18,46 @@ player = player_car.Player()
 enemy = enemy_cars.Enemy()
 coin = collectable.Coin()
 
-coins = pygame.sprite.Group()
+coins = pg.sprite.Group()
 coins.add(coin)
 
-enemies = pygame.sprite.Group()
+enemies = pg.sprite.Group()
 enemies.add(enemy)
-all_sprites = pygame.sprite.Group()
+all_sprites = pg.sprite.Group()
 all_sprites.add(move_road)
 all_sprites.add(player)
 all_sprites.add(enemy)
 all_sprites.add(coin)
 
-grass = pygame.image.load("assets/grass.png")
+grass = pg.image.load("assets/grass.png")
 
 score = 0
-font = pygame.font.Font("assets/ARCADECLASSIC.ttf", 72, bold=True)
+font = pg.font.Font("assets/ARCADECLASSIC.ttf", 72, bold=True)
 
 WHITE = (255,255,255)
 RED = (255,0,0)
 menu.draw(screen,WHITE)
 
 speed = 5
-
 timer = 0
 
 coin_collected = False
 
 done = False
 while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+    for event in pg.event.get():
+        if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             done = True
             sys.exit()
 
+    #draw grass
     screen.blit(grass,(0,0))
-      
 
+    #draw score
     text = font.render(str(score), True, WHITE)
     screen.blit(text,(100,480))
 
+    #draw every sprite and start moving
     for entity in all_sprites:
         entity.draw(screen)
         try:
@@ -63,23 +65,22 @@ while not done:
         except TypeError:
             entity.move(speed)
 
+    #change speed with time
     timer += 1
-
     if (timer / 100) % 5 == 0 and speed <= 11:
         speed += 2
-        
 
-    if pygame.sprite.spritecollideany(player, coins):
+    #coin collision
+    if pg.sprite.spritecollideany(player, coins):
         all_sprites.remove(coin)
         coin = collectable.Coin()
         all_sprites.add(coin)
-        coins = pygame.sprite.Group()
+        coins = pg.sprite.Group()
         coins.add(coin)
         score += 1
-       
-    #all_sprites.add(coin)
         
-    if pygame.sprite.spritecollideany(player, enemies):
+    #enemy collision and reset
+    if pg.sprite.spritecollideany(player, enemies):
         for entity in all_sprites:
             entity.kill()
 
@@ -94,18 +95,18 @@ while not done:
         player = player_car.Player()
         enemy = enemy_cars.Enemy()
         coin = collectable.Coin()
-        enemies = pygame.sprite.Group()
+        enemies = pg.sprite.Group()
         enemies.add(enemy)
-        all_sprites = pygame.sprite.Group()
+        all_sprites = pg.sprite.Group()
         all_sprites.add(move_road)
         all_sprites.add(player)
         all_sprites.add(enemy)
         all_sprites.add(coin)
 
-        coins = pygame.sprite.Group()
+        coins = pg.sprite.Group()
         coins.add(coin)
         speed = 5
         score = 0
-    
-    pygame.display.flip()
+
+    pg.display.flip()
     FPS.tick(60)
