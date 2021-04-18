db08fd88af592acadd2852ed3ff2dd635c1eff50
Alikhan Bissakov <bissakov@yandex.ru>
2021-04-18 15:49:40 +0600

    .

--- TSIS9/Snake/start_screen.py
+++ TSIS9/Snake/start_screen.py
@@ -1,22 +1,30 @@
 import sys
 import pygame
+from variables import *
 
 class Menu(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.in_menu = True
-        self.rect = pygame.Rect(563,372,155,57)
+        self.rect = pygame.Rect(550,372,180,57)
         self.start = 1
-        self.WHITE = (255,255,255)
-        self.BLACK = (0,0,0)
-        self.BLUE = (0,0,255)
-        self.YELLOW = (255,255,0)
 
     def menuAnnotations(self,surface,s,y,color,size):
         font = pygame.font.Font("arial.ttf", size)
         text = font.render(s, True, color)
         text_rect = text.get_rect(center=(640, y))
         surface.blit(text,text_rect)
+    
+    def scale_buttons(self):
+        if self.rect.y == 372:
+            self.rect.w = 180
+            self.rect.x = (1280 - self.rect.w) / 2
+        elif self.rect.y == 452:
+            self.rect.w = 300
+            self.rect.x = (1280 - self.rect.w) / 2
+        elif self.rect.y == 532:
+            self.rect.w = 130
+            self.rect.x = (1280 - self.rect.w) / 2
 
     def draw(self,surface,color,SCORE,HIGHSCORE):
         while self.in_menu:
@@ -35,24 +43,30 @@ class Menu(pygame.sprite.Sprite):
                             sys.exit()
                     if event.key == pygame.K_DOWN:
                         self.rect.move_ip(0,80)
+                        if self.rect.y > 532:
+                            self.rect.move_ip(0,-240)
+                        self.scale_buttons()
                     elif event.key == pygame.K_UP:
                         self.rect.move_ip(0,-80)
+                        if self.rect.y < 372:
+                            self.rect.move_ip(0,240)
+                        self.scale_buttons()
             surface.fill(color)
 
             if color == (255,0,0):
-                pygame.draw.line(surface, self.BLACK, (385,350), (890,350), 10)
-                pygame.draw.line(surface, self.BLACK, (385,250), (890,250), 10)
-                self.menuAnnotations(surface,"GAME    OVER",300,self.BLACK,96)
+                pygame.draw.line(surface, BLACK, (385,350), (890,350), 10)
+                pygame.draw.line(surface, BLACK, (385,250), (890,250), 10)
+                self.menuAnnotations(surface,"GAME    OVER",300,BLACK,96)
                 highscore = "Highscore                   " + str(HIGHSCORE)
-                self.menuAnnotations(surface,highscore,100,self.YELLOW,48)
+                self.menuAnnotations(surface,highscore,100,YELLOW,48)
                 score = "Current Score     " + str(SCORE)
-                self.menuAnnotations(surface,score,170,self.YELLOW,48)
+                self.menuAnnotations(surface,score,170,YELLOW,48)
                 if self.start:
-                    self.menuAnnotations(surface,"CONTINUE",440,self.WHITE,56)
-                    self.menuAnnotations(surface,"EXIT",520,self.BLACK,56)
+                    self.menuAnnotations(surface,"CONTINUE",440,WHITE,56)
+                    self.menuAnnotations(surface,"EXIT",520,BLACK,56)
                 else:
-                    self.menuAnnotations(surface,"CONTINUE",440,self.BLACK,56)
-                    self.menuAnnotations(surface,"EXIT",520,self.WHITE,56)
+                    self.menuAnnotations(surface,"CONTINUE",440,BLACK,56)
+                    self.menuAnnotations(surface,"EXIT",520,WHITE,56)
             else:
                 # if self.start:
                 #     self.menuAnnotations(surface,"START",400,self.BLUE,56)
@@ -62,9 +76,9 @@ class Menu(pygame.sprite.Sprite):
                 #     self.menuAnnotations(surface,"START",400,self.BLUE,56)
                 #     self.menuAnnotations(surface,"CONTINUE",480,self.BLUE,56)
                 #     self.menuAnnotations(surface,"EXIT",560,self.BLUE,56)
-                pygame.draw.rect(surface,self.BLUE,self.rect)
-                self.menuAnnotations(surface,"START",400,self.BLACK,56)
-                self.menuAnnotations(surface,"CONTINUE",480,self.BLACK,56)
-                self.menuAnnotations(surface,"EXIT",560,self.BLACK,56)
+                pygame.draw.rect(surface,BLUE,self.rect)
+                self.menuAnnotations(surface,"START",400,BLACK,56)
+                self.menuAnnotations(surface,"CONTINUE",480,BLACK,56)
+                self.menuAnnotations(surface,"EXIT",560,BLACK,56)
                 
             pygame.display.flip()
